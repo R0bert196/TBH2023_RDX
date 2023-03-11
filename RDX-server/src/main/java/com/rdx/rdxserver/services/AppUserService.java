dae1b53cfa9ab79c209644764f23913fef8974fa
R0bert196 <robertchiriac196@yahoo.com>
2023-03-11 14:39:19 +0200

    Create appUser service and repo

---
+++ RDX-server/src/main/java/com/rdx/rdxserver/services/AppUserService.java
@@ -0,0 +1,14 @@
+package com.rdx.rdxserver.services;
+
+import com.rdx.rdxserver.repositories.AppUserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserService {
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+}
