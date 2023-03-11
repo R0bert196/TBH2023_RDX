b6270eacc5527addceec99916d77decef7061faa
R0bert196 <robertchiriac196@yahoo.com>
2023-03-11 15:20:04 +0200

    Add findByEmail method

--- RDX-server/src/main/java/com/rdx/rdxserver/repositories/AppUserRepository.java
+++ RDX-server/src/main/java/com/rdx/rdxserver/repositories/AppUserRepository.java
@@ -4,7 +4,10 @@ import com.rdx.rdxserver.entities.AppUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUserEntity, Integer> {
 
+    Optional<AppUserEntity> findByEmail(String email);
 }
