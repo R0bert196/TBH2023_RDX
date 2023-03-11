d330fbdfc87f5e528ffe73d21838ee655cf2e732
R0bert196 <robertchiriac196@yahoo.com>
2023-03-11 13:06:46 +0200

    initial commit

---
+++ RDX-server/src/main/java/com/rdx/rdxserver/RdxServerApplication.java
@@ -0,0 +1,13 @@
+package com.rdx.rdxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdxServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdxServerApplication.class, args);
+    }
+
+}
---
+++ RDX-server/src/test/java/com/rdx/rdxserver/RdxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdx.rdxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
