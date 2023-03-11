f429f4018e271363e5a2a4517932901258473493
R0bert196 <robertchiriac196@yahoo.com>
2023-03-11 22:51:21 +0200

    Implement logic to registerContract and save a contract in the db

--- RDX-server/src/main/java/com/rdx/rdxserver/services/ContractService.java
+++ RDX-server/src/main/java/com/rdx/rdxserver/services/ContractService.java
@@ -1,6 +1,10 @@
 package com.rdx.rdxserver.services;
 
+import com.rdx.rdxserver.entities.AppUserEntity;
+import com.rdx.rdxserver.entities.CompanyEntity;
 import com.rdx.rdxserver.entities.ContractEntity;
+import com.rdx.rdxserver.entities.WalletEntity;
+import com.rdx.rdxserver.repositories.AppUserRepository;
 import com.rdx.rdxserver.repositories.ContractRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,25 +15,40 @@ import java.util.Optional;
 public class ContractService {
 
     private final ContractRepository contractRepository;
+    private final AppUserRepository appUserRepository;
+    private final WalletService walletService;
 
 
-    public ContractService(ContractRepository contractRepository) {
+    public ContractService(ContractRepository contractRepository, AppUserRepository appUserRepository, WalletService walletService) {
         this.contractRepository = contractRepository;
+        this.appUserRepository = appUserRepository;
+        this.walletService = walletService;
     }
 
     public ContractEntity getContractById(int id) {
         return contractRepository.findById(id).orElse(null);
     }
 
-    public ContractEntity registerContract(ContractEntity tempContractEntity) {
-        if (contractRepository.findById(tempContractEntity.getId()).isPresent()) return null;
-        //TODO: CREATE CONTRACT WALLET !
-//        WalletEntity walletEntity = new WalletEntity();
+    public ContractEntity registerContract(ContractEntity tempContractEntity, AppUserEntity user) {
+//        if (contractRepository.findById(tempContractEntity.getId()).isPresent()) {
+//            return null;
+//        }
+
+
+        //TODO: calculam din temp contract value #cvuri : ex pt 10 egold dam 20 cvuri..
+        // ceren ka embeding service sa ne dea cele mai asemenatoare #cvuri care se aseamana cu embedingul contractului
+//        List<AppUserEntity> useriPotriviti = embeddingService.sortBy(tempContractEntity.getEmbeddingsEntity().getValues(),tempContractEntity.getBudget())
+        CompanyEntity company = user.getCompanyEntity();
+        WalletEntity wallet = walletService.createAndSaveWallet();
+
+
         ContractEntity newContract = ContractEntity
                 .builder()
                 .profileText(tempContractEntity.getProfileText())
-                .companyEntity(tempContractEntity.getCompanyEntity())
-//                .wallet(walletEntity)
+                .budget(tempContractEntity.getBudget())
+                .companyEntity(company)
+                .walletEntity(wallet)
+                .paid(false)
                 .build();
 
         //TODO: ADD EMBEDDINGS
