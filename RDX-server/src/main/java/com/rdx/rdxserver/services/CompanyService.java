74eaf3996607f9d18dc0bced4f6657a2f8ca5dcd
R0bert196 <robertchiriac196@yahoo.com>
2023-03-11 14:39:06 +0200

    Create company service and repo

---
+++ RDX-server/src/main/java/com/rdx/rdxserver/services/CompanyService.java
@@ -0,0 +1,14 @@
+package com.rdx.rdxserver.services;
+
+import com.rdx.rdxserver.repositories.CompanyRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+}
