af38176f5d4a412cf6b446ddfd5988da0f88aa65
Dragos B <89776157+dragosbas@users.noreply.github.com>
2023-03-11 16:18:18 +0200

    Added Contract Controller

--- RDX-server/src/main/java/com/rdx/rdxserver/repositories/ContractRepository.java
+++ RDX-server/src/main/java/com/rdx/rdxserver/repositories/ContractRepository.java
@@ -4,6 +4,9 @@ import com.rdx.rdxserver.entities.ContractEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContractRepository extends JpaRepository<ContractEntity, Integer> {
+    List<ContractEntity> findByCompanyEntity_Id(int companyId);
 }
