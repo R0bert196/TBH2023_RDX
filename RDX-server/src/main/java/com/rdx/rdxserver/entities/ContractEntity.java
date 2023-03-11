f342be6ac186b0b1a64bc55fcbdf88c491433a47
R0bert196 <robertchiriac196@yahoo.com>
2023-03-11 20:34:10 +0200

    add default value of paid to false

--- RDX-server/src/main/java/com/rdx/rdxserver/entities/ContractEntity.java
+++ RDX-server/src/main/java/com/rdx/rdxserver/entities/ContractEntity.java
@@ -30,7 +30,7 @@ public class ContractEntity {
     // user sa leg ca lista
     //embedding sa le leg
     private String profileText;
-    private Boolean paid;
+    private Boolean paid = false;
     @OneToOne(orphanRemoval = true)
     @JoinColumn(name = "profile_embeddings_id", unique = true, referencedColumnName = "id")
     private EmbeddingsEntity embeddingsEntity;
