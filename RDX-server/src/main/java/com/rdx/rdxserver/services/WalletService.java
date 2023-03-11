d64992461adfade44f00dad93c62c0c98d6ba383
R0bert196 <robertchiriac196@yahoo.com>
2023-03-11 22:50:20 +0200

    Create mock method to create a random wallet

--- RDX-server/src/main/java/com/rdx/rdxserver/services/WalletService.java
+++ RDX-server/src/main/java/com/rdx/rdxserver/services/WalletService.java
@@ -1,7 +1,11 @@
 package com.rdx.rdxserver.services;
 
+import com.rdx.rdxserver.entities.WalletEntity;
 import com.rdx.rdxserver.repositories.WalletRepository;
 import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.stream.Collectors;
 
 @Service
 public class WalletService {
@@ -10,5 +14,17 @@ public class WalletService {
 
     public WalletService(WalletRepository walletRepository) {
         this.walletRepository = walletRepository;
+    }
+
+    public WalletEntity createAndSaveWallet() {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom random = new SecureRandom();
+        String walletAddress =  random.ints(30, 0, chars.length())
+                .mapToObj(i -> String.valueOf(chars.charAt(i)))
+                .collect(Collectors.joining());
+        WalletEntity wallet = WalletEntity.builder()
+                        .address(walletAddress)
+                        .build();
+        return walletRepository.save(wallet);
     }
 }
