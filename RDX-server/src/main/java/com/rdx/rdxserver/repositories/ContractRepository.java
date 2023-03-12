0c3bf09e188e83d2c301676ddc851dc8f5080d2a
Dragos B <89776157+dragosbas@users.noreply.github.com>
2023-03-12 11:42:04 +0200

    Added get contracts

--- RDX-server/src/main/java/com/rdx/rdxserver/repositories/ContractRepository.java
+++ RDX-server/src/main/java/com/rdx/rdxserver/repositories/ContractRepository.java
@@ -1,9 +1,11 @@
 package com.rdx.rdxserver.repositories;
 
+import com.rdx.rdxserver.entities.AppUserEntity;
 import com.rdx.rdxserver.entities.ContractAppUserEntity;
 import com.rdx.rdxserver.entities.ContractEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +17,6 @@ public interface ContractRepository extends JpaRepository<ContractEntity, Intege
 
     @Query(value = "select * from contract inner join contract_appuser ca on contract.id = ca.contract_id where ca.user_id = :userId;", nativeQuery = true)
     List<ContractEntity> findAllByUserId(int userId);
-
+    @Query("SELECT cu.appUser FROM ContractAppUserEntity cu WHERE cu.contract.id = :contractId")
+    List<AppUserEntity> findAppUsersByContractId(@Param("contractId") Integer contractId);
 }
