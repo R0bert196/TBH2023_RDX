3957a681b8d85e7456cb6bfcd5a9fe8e00efa719
R0bert196 <robertchiriac196@yahoo.com>
2023-03-12 05:57:04 +0200

    Create method that finds all ContractAppUserEntities with an app user id

--- RDX-server/src/main/java/com/rdx/rdxserver/repositories/ContractAppUserRepository.java
+++ RDX-server/src/main/java/com/rdx/rdxserver/repositories/ContractAppUserRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ContractAppUserRepository extends JpaRepository<ContractAppUserEntity, ContractAppUserPk> {
 
 
     @Query(value = "select * from contract_appuser where contract_id = :contractId and cosine_similarity > 0.5",nativeQuery = true)
     List<ContractAppUserEntity> findAllByContractId(int contractId);
+
+    Optional<ContractAppUserEntity> findByAppUser_Id(int appUserId);
 }
